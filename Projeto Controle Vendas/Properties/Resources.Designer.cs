9b7c518209204e8383cca74d7ce1c03979ceea8a
Juliano Candido <julianosgs@yahoo.com.br>
2023-02-01 14:39:43 -0300

    Inicio criação tela de Login e tela principal

--- Projeto Controle Vendas/Properties/Resources.Designer.cs
+++ Projeto Controle Vendas/Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Projeto_Controle_Vendas.Properties
-{
-
-
+namespace Projeto_Controle_Vendas.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -19,52 +19,134 @@ namespace Projeto_Controle_Vendas.Properties
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Projeto_Controle_Vendas.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap _1481080293_Money {
+            get {
+                object obj = ResourceManager.GetObject("1481080293_Money", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap bannerVenda {
+            get {
+                object obj = ResourceManager.GetObject("bannerVenda", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap cad_produtos_40x40 {
+            get {
+                object obj = ResourceManager.GetObject("cad_produtos 40x40", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap cadastro_clientes_40_x_40 {
+            get {
+                object obj = ResourceManager.GetObject("cadastro clientes 40 x 40", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap configurações {
+            get {
+                object obj = ResourceManager.GetObject("configurações", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap couple64 {
+            get {
+                object obj = ResourceManager.GetObject("couple64", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap fundo_sistema {
+            get {
+                object obj = ResourceManager.GetObject("fundo sistema", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap fundo_sistema_azul {
+            get {
+                object obj = ResourceManager.GetObject("fundo sistema azul", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap usuarios_40x40 {
+            get {
+                object obj = ResourceManager.GetObject("usuarios 40x40", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
---
+++ Projeto Controle Vendas/Views/Frmmenu.Designer.cs
@@ -0,0 +1,334 @@
+﻿namespace Projeto_Controle_Vendas.Views
+{
+    partial class Frmmenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menuCliente = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuCadClientes = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuConClientes = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuFuncionarios = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuCadFuncionarios = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuConsFuncionarios = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuFornecedores = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuCadFornecedores = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuConsFornecedores = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuProdutos = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuCadProdutos = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuConsProdutos = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuVendas = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuNovaVenda = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuHistoricoVenda = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuConfiguracoes = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuTrocarUsuario = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuSairSistema = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.txtData = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel3 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.txtHora = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel5 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.txtUsuario = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuCliente,
+            this.menuFuncionarios,
+            this.menuFornecedores,
+            this.menuProdutos,
+            this.menuVendas,
+            this.menuConfiguracoes});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(941, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menuCliente
+            // 
+            this.menuCliente.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuCadClientes,
+            this.menuConClientes});
+            this.menuCliente.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuCliente.Image = global::Projeto_Controle_Vendas.Properties.Resources.cadastro_clientes_40_x_40;
+            this.menuCliente.Name = "menuCliente";
+            this.menuCliente.Size = new System.Drawing.Size(89, 24);
+            this.menuCliente.Text = "Clientes";
+            // 
+            // menuCadClientes
+            // 
+            this.menuCadClientes.Name = "menuCadClientes";
+            this.menuCadClientes.Size = new System.Drawing.Size(214, 24);
+            this.menuCadClientes.Text = "Cadastro de Clientes";
+            // 
+            // menuConClientes
+            // 
+            this.menuConClientes.Name = "menuConClientes";
+            this.menuConClientes.Size = new System.Drawing.Size(214, 24);
+            this.menuConClientes.Text = "Consulta de Clientes";
+            // 
+            // menuFuncionarios
+            // 
+            this.menuFuncionarios.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuCadFuncionarios,
+            this.menuConsFuncionarios});
+            this.menuFuncionarios.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuFuncionarios.Image = global::Projeto_Controle_Vendas.Properties.Resources.usuarios_40x40;
+            this.menuFuncionarios.Name = "menuFuncionarios";
+            this.menuFuncionarios.Size = new System.Drawing.Size(120, 24);
+            this.menuFuncionarios.Text = "Funcionários";
+            // 
+            // menuCadFuncionarios
+            // 
+            this.menuCadFuncionarios.Name = "menuCadFuncionarios";
+            this.menuCadFuncionarios.Size = new System.Drawing.Size(245, 24);
+            this.menuCadFuncionarios.Text = "Cadastro de Funcionários";
+            // 
+            // menuConsFuncionarios
+            // 
+            this.menuConsFuncionarios.Name = "menuConsFuncionarios";
+            this.menuConsFuncionarios.Size = new System.Drawing.Size(245, 24);
+            this.menuConsFuncionarios.Text = "Consulta de Funcionários";
+            // 
+            // menuFornecedores
+            // 
+            this.menuFornecedores.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuCadFornecedores,
+            this.menuConsFornecedores});
+            this.menuFornecedores.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuFornecedores.Image = global::Projeto_Controle_Vendas.Properties.Resources.couple64;
+            this.menuFornecedores.Name = "menuFornecedores";
+            this.menuFornecedores.Size = new System.Drawing.Size(126, 24);
+            this.menuFornecedores.Text = "Fornecedores";
+            // 
+            // menuCadFornecedores
+            // 
+            this.menuCadFornecedores.Name = "menuCadFornecedores";
+            this.menuCadFornecedores.Size = new System.Drawing.Size(251, 24);
+            this.menuCadFornecedores.Text = "Cadastro de Fornecedores";
+            // 
+            // menuConsFornecedores
+            // 
+            this.menuConsFornecedores.Name = "menuConsFornecedores";
+            this.menuConsFornecedores.Size = new System.Drawing.Size(251, 24);
+            this.menuConsFornecedores.Text = "Consulta de Fornecedores";
+            // 
+            // menuProdutos
+            // 
+            this.menuProdutos.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuCadProdutos,
+            this.menuConsProdutos});
+            this.menuProdutos.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuProdutos.Image = global::Projeto_Controle_Vendas.Properties.Resources.cad_produtos_40x40;
+            this.menuProdutos.Name = "menuProdutos";
+            this.menuProdutos.Size = new System.Drawing.Size(96, 24);
+            this.menuProdutos.Text = "Produtos";
+            // 
+            // menuCadProdutos
+            // 
+            this.menuCadProdutos.Name = "menuCadProdutos";
+            this.menuCadProdutos.Size = new System.Drawing.Size(221, 24);
+            this.menuCadProdutos.Text = "Cadastro de Produtos";
+            // 
+            // menuConsProdutos
+            // 
+            this.menuConsProdutos.Name = "menuConsProdutos";
+            this.menuConsProdutos.Size = new System.Drawing.Size(221, 24);
+            this.menuConsProdutos.Text = "Consulta de Produtos";
+            // 
+            // menuVendas
+            // 
+            this.menuVendas.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuNovaVenda,
+            this.menuHistoricoVenda});
+            this.menuVendas.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuVendas.Image = global::Projeto_Controle_Vendas.Properties.Resources._1481080293_Money;
+            this.menuVendas.Name = "menuVendas";
+            this.menuVendas.Size = new System.Drawing.Size(84, 24);
+            this.menuVendas.Text = "Vendas";
+            // 
+            // menuNovaVenda
+            // 
+            this.menuNovaVenda.Name = "menuNovaVenda";
+            this.menuNovaVenda.Size = new System.Drawing.Size(210, 24);
+            this.menuNovaVenda.Text = "Nova Venda";
+            // 
+            // menuHistoricoVenda
+            // 
+            this.menuHistoricoVenda.Name = "menuHistoricoVenda";
+            this.menuHistoricoVenda.Size = new System.Drawing.Size(210, 24);
+            this.menuHistoricoVenda.Text = "Histórico de Vendas";
+            // 
+            // menuConfiguracoes
+            // 
+            this.menuConfiguracoes.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuTrocarUsuario,
+            this.menuSairSistema});
+            this.menuConfiguracoes.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuConfiguracoes.Image = global::Projeto_Controle_Vendas.Properties.Resources.configurações;
+            this.menuConfiguracoes.Name = "menuConfiguracoes";
+            this.menuConfiguracoes.Size = new System.Drawing.Size(132, 24);
+            this.menuConfiguracoes.Text = "Configurações";
+            // 
+            // menuTrocarUsuario
+            // 
+            this.menuTrocarUsuario.Name = "menuTrocarUsuario";
+            this.menuTrocarUsuario.Size = new System.Drawing.Size(192, 24);
+            this.menuTrocarUsuario.Text = "Trocar de usuário";
+            // 
+            // menuSairSistema
+            // 
+            this.menuSairSistema.Name = "menuSairSistema";
+            this.menuSairSistema.Size = new System.Drawing.Size(192, 24);
+            this.menuSairSistema.Text = "Sair do Sistema";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.txtData,
+            this.toolStripStatusLabel3,
+            this.txtHora,
+            this.toolStripStatusLabel5,
+            this.txtUsuario});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 486);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(941, 25);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripStatusLabel1.ForeColor = System.Drawing.Color.Red;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(84, 20);
+            this.toolStripStatusLabel1.Text = "Data Atual";
+            // 
+            // txtData
+            // 
+            this.txtData.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtData.ForeColor = System.Drawing.Color.Red;
+            this.txtData.Name = "txtData";
+            this.txtData.Size = new System.Drawing.Size(95, 20);
+            this.txtData.Text = "01/02/2023";
+            // 
+            // toolStripStatusLabel3
+            // 
+            this.toolStripStatusLabel3.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripStatusLabel3.ForeColor = System.Drawing.Color.Red;
+            this.toolStripStatusLabel3.Name = "toolStripStatusLabel3";
+            this.toolStripStatusLabel3.Size = new System.Drawing.Size(85, 20);
+            this.toolStripStatusLabel3.Text = "Hora Atual";
+            // 
+            // txtHora
+            // 
+            this.txtHora.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHora.ForeColor = System.Drawing.Color.Red;
+            this.txtHora.Name = "txtHora";
+            this.txtHora.Size = new System.Drawing.Size(49, 20);
+            this.txtHora.Text = "15:00";
+            // 
+            // toolStripStatusLabel5
+            // 
+            this.toolStripStatusLabel5.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStripStatusLabel5.ForeColor = System.Drawing.Color.Red;
+            this.toolStripStatusLabel5.Name = "toolStripStatusLabel5";
+            this.toolStripStatusLabel5.Size = new System.Drawing.Size(115, 20);
+            this.toolStripStatusLabel5.Text = "Usuário logado";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuario.ForeColor = System.Drawing.Color.Red;
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(59, 20);
+            this.txtUsuario.Text = "Juliano";
+            // 
+            // Frmmenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.BackgroundImage = global::Projeto_Controle_Vendas.Properties.Resources.bannerVenda;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(941, 511);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Frmmenu";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Menu Principal - Controle de Vendas";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menuCliente;
+        private System.Windows.Forms.ToolStripMenuItem menuCadClientes;
+        private System.Windows.Forms.ToolStripMenuItem menuConClientes;
+        private System.Windows.Forms.ToolStripMenuItem menuFuncionarios;
+        private System.Windows.Forms.ToolStripMenuItem menuCadFuncionarios;
+        private System.Windows.Forms.ToolStripMenuItem menuConsFuncionarios;
+        private System.Windows.Forms.ToolStripMenuItem menuFornecedores;
+        private System.Windows.Forms.ToolStripMenuItem menuCadFornecedores;
+        private System.Windows.Forms.ToolStripMenuItem menuConsFornecedores;
+        private System.Windows.Forms.ToolStripMenuItem menuProdutos;
+        private System.Windows.Forms.ToolStripMenuItem menuCadProdutos;
+        private System.Windows.Forms.ToolStripMenuItem menuConsProdutos;
+        private System.Windows.Forms.ToolStripMenuItem menuVendas;
+        private System.Windows.Forms.ToolStripMenuItem menuNovaVenda;
+        private System.Windows.Forms.ToolStripMenuItem menuHistoricoVenda;
+        private System.Windows.Forms.ToolStripMenuItem menuConfiguracoes;
+        private System.Windows.Forms.ToolStripMenuItem menuTrocarUsuario;
+        private System.Windows.Forms.ToolStripMenuItem menuSairSistema;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel txtData;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel3;
+        private System.Windows.Forms.ToolStripStatusLabel txtHora;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel5;
+        private System.Windows.Forms.ToolStripStatusLabel txtUsuario;
+    }
+}
